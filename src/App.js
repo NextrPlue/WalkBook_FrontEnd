4a3fca01c6449200b2b7540feb2675fff347a1fa
doyooon <ian661087@gmail.com>
2025-05-30 14:37:49 +0900

    feat: image 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import BookDetailPage from './pages/BookDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
+import BookFormPage from './pages/BookFormPage';
+
 // import BookFormPage from './pages/BookFormPage'; // 나중에 팀원이 추가할 때 주석 해제
 import './App.css';
 
@@ -14,7 +16,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/book/:id" element={<BookDetailPage />} />
           {/* 나중에 팀원이 추가할 라우트들 */}
-          {/* <Route path="/book/new" element={<BookFormPage />} /> */}
+          <Route path="/book/new" element={<BookFormPage />} />
           {/* <Route path="/book/edit/:id" element={<BookFormPage />} /> */}
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
