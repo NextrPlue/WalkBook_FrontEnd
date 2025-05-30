94282109bf7cc45e0de849065e6fdedcf2e01d4a
NexTR_Plue <p.plue1881@gmail.com>
2025-05-30 11:34:22 +0900

    feat: 라우팅 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import BookDetailPage from './pages/BookDetailPage';
+import NotFoundPage from './pages/NotFoundPage';
+// import BookFormPage from './pages/BookFormPage'; // 나중에 팀원이 추가할 때 주석 해제
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/book/:id" element={<BookDetailPage />} />
+          {/* 나중에 팀원이 추가할 라우트들 */}
+          {/* <Route path="/book/new" element={<BookFormPage />} /> */}
+          {/* <Route path="/book/edit/:id" element={<BookFormPage />} /> */}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
